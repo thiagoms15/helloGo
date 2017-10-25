83af7891848f33b018718dddd52bf6adf47f8eb6
Thiago Marques <thiagoms.15@gmail.com>
2017-10-24 22:13:20 -0200

    Add new informations about how to use GO Add functions

--- main.go
+++ main.go
@@ -4,7 +4,33 @@ package main
 import "fmt"
 
 func main() {
+	var name string
+	name = "Thiago"
 	fmt.Println("Hello World")
-	fmt.Println("Hello, my name is Thiago")
+	fmt.Println("Hello, my name is " + name)
+	fmt.Println("Sum is", add(12,10))
+	readInput()
+
+	for i := 1; i <= 5; i++ {
+	  if i % 2 == 0 {
+        // even
+        fmt.Println(i, "is even")
+      } else {
+        // odd
+        fmt.Println(i, "is odd")
+      }
+  }
+}
+
+func add(a int, b int)int {
+	return a + b
+}
+
+func readInput() {
+	var input float64
 	
+	fmt.Print("Enter a number: ")
+	fmt.Scanf("%f", &input)
+	fmt.Println("The number is", input)
+
 }
